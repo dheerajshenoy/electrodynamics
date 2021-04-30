be9581634d0cbf7a05cd38a5df4e12efe1f0ff23
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2021-04-30 17:49:09 +0530

    Add files via upload

--- particle_motion.py
+++ particle_motion.py
@@ -9,7 +9,7 @@ def E(r0, x, y):
     return r0[0] * (x - r0[1])/denom, r0[0] * (y - r0[2])/denom
 
 fig, ax = plt.subplots()
-WIDTH, HEIGHT = 4, 4
+WIDTH, HEIGHT = 10, 10
 ax.set_xlim(-WIDTH, WIDTH)
 ax.set_ylim(-HEIGHT, HEIGHT)
 
@@ -19,17 +19,21 @@ X, Y = np.meshgrid(x, y)
 
 def animate(frame):
     Ex, Ey = np.zeros((ny, nx)), np.zeros((ny, nx))
+    ax.clear()
     ax.collections = []
     ax.patches = []
-    charges = [[1e-10,1, 1], [-1e-10, -1*frame/10, 1*frame/10]]
+    charges = [[1e-6, 0, 0], [-1e-6, 2*np.pi*np.cos(frame/100), -2*np.pi*np.sin(frame/100)]]
     for charge in charges:
         ex, ey = E(charge, x=X, y=Y)
         Ex += ex
         Ey += ey
-        ax.add_artist(Circle(radius=0.1, xy=(charge[1],charge[2])))
+        ax.add_artist(Circle(radius=0.5, xy=(charge[1], charge[2])))
     color = 2 * np.log(np.hypot(Ex, Ey))
-    ln = ax.streamplot(X, Y, Ex, Ey, density=2, arrowstyle='->', linewidth=1, arrowsize=1.5, cmap=plt.cm.inferno)
+    ln = ax.streamplot(X, Y, Ex, Ey, density=1, arrowstyle='->', linewidth=1, arrowsize=0.8)
+    print(frame)
     return ln
 
-anim = FuncAnimation(fig, animate, frames=100, interval=50, blit=False, repeat=False)
-anim.save('animation.gif', writer='imagemagick')
+anim = FuncAnimation(fig, animate, frames=10, interval=50, repeat=True)
+#anim.save('animation.gif', writer='imagemagick')
+plt.show()
+print("DONE")
